2735a208ab53c808f819b389f1cba03eeaf34f1c
Annchousinka <marylyann@mail.ru>
2023-02-26 21:33:21 +0300

    task_final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,19 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: " ", order: {address: " ", sum}, goods: []};
 
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    let clientData = `${name} ${phone}`;
+    let addressData = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    let goodsData = [];
+        for (let i = 0; i < goods.length; i++){
+            goodsData.push({title: goods[i].title, count: goods[i].count})
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.client = clientData;
+    data.order.address = addressData;
+    data.order.sum = sum;
+    data.goods = goodsData;
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
+    return JSON.stringify({data});
 }
